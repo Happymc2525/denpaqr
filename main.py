0b003829a98d012a5f0a99fdbe6f32a28a825400
はっぴー <60919012+Happmc2525@users.noreply.github.com>
2025-03-10 10:46:09 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord import Option
+import random
+import string
+import qrcode
+
+TOKEN = 'ここにBotトークン'
+bot = discord.Bot(intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} 起動しました")
+
+@bot.slash_command(description="QRコード生成")
+async def qr(
+  ctx: discord.ApplicationContext,
+  data: Option(str, required=False, description="データ指定")
+):
+  data = data or (''.join(random.choices(string.ascii_letters + string.digits, k=16)))
+  img = qrcode.make(data)
+  img.save('qr.png')
+  await ctx.respond(f'{data}', file=discord.File('qr.png'))
+
+@bot.slash_command(description="ランダムQRコード(プライベート)")
+async def sqr(
+  ctx: discord.ApplicationContext,
+  data: Option(str, required=False, description="データ指定")
+):
+  data = data or (''.join(random.choices(string.ascii_letters + string.digits, k=16)))
+  img = qrcode.make(data)
+  img.save('qr.png')
+  await ctx.respond(f'{data}', file=discord.File('qr.png'), ephemeral=True)
+
+bot.run(TOKEN)
